66b6ad03429d77cd0336f193b6e4bb04316b3934
negri234279 <rafalejo@live.com>
2022-06-24 14:57:17 +0200

    👔 ➕ Add morgan and cors and add a create user

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 require('dotenv').config() 
 const express = require('express')
+const morgan = require('morgan')
 const { Pool } = require('pg')
 
 const PORT = process.env.APP_PORT
 
 const app = express()
+app.use(express.json())
+app.use(morgan('dev'))
 
 const pool = new Pool({
     user: process.env.DB_USER,
@@ -14,12 +17,26 @@ const pool = new Pool({
     database: process.env.DB_NAME
 })
 
-app.get('/', async (req, res) => {
+app.get('/api/user', async (req, res) => {
     try {
         const data = await pool.query('SELECT * FROM users')
         res.json(data.rows)
     } catch (error) {
         res.status(404).send('Error')
+    }
+})
+
+app.post('/api/user', async (req, res) => {
+    const { name, email, password } = req.body
+
+    try {
+        await pool.query(
+            'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
+            [name, email, password]
+        )
+        res.status(201).send()
+    } catch (error) {
+        res.status(400).send('Error')
     }
 })
 
